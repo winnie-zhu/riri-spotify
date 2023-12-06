d4ce11b3be072d6c4863a86a020ae51f8e83de57
Sara <sara.muzaffar@gmail.com>
2023-12-06 03:29:53 -0800

    api and results working on website

--- frontend/pages/Home.jsx
+++ frontend/pages/Home.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom"; //
 
 const extractURLParams = (url) => {
   const params = new URLSearchParams(url.split("#")[1]);
@@ -12,6 +12,12 @@ const extractURLParams = (url) => {
 
 function Home() {
   const [loggedIn, setLoggedIn] = useState(false);
+
+  //change 2
+  const [targetVariance, setTargetVariance] = useState("");
+  const navigate = useNavigate();
+  //
+  
 
   useEffect(() => {
     if (window.location.hash) {
@@ -33,20 +39,42 @@ function Home() {
       setLoggedIn(true);
     }
   }
+  //change 3 and 4
+  const handleInputChange = (event) => {
+    // Validate and normalize the input to ensure it's a valid number between 0 and 1
+    const inputValue = parseFloat(event.target.value);
+    if (!isNaN(inputValue) && inputValue >= 0 && inputValue <= 1) {
+      setTargetVariance(inputValue);
+    }
+  };
+
+  const handleLinkClick = () => {
+    // Validate the input or perform any other necessary checks
+    if (targetVariance !== "") {
+      // Navigate to the Results component with the targetVariance in state
+      navigate("/results", { state: { targetVariance: targetVariance } });
+    } else {
+      // Handle invalid input or show an error message
+      console.error("Invalid input for targetVariance");
+    }
+  };
+  
+  //end change 3 and 4
 
   const handleLogout = () => {
     localStorage.clear();
     setLoggedIn(false);
   };
-
+  //<Link to="/results">Results</Link>
   return (
     <>
       <h1>Home</h1>
       {loggedIn ? (
         <>
-          Enter your mood
-          <input type="text" />
-          <Link to="/results">Results</Link>
+          Enter your mood (0 to 1):
+          <input type="number" step="0.1" value={targetVariance} onChange={handleInputChange} />
+          {/* Use a button or any other UI element to trigger the link */}
+          <button onClick={handleLinkClick}>Go to Results</button>
           <button onClick={handleLogout}>Logout</button>
         </>
       ) : (
--- frontend/pages/Result.jsx
+++ frontend/pages/Result.jsx
@@ -11,13 +11,31 @@ function Result() {
       try {
         const accessToken = localStorage.getItem('accessToken');
 
-        const response = await axios.get("http://localhost:8888/results", {
-            headers: {
-                Authorization: `Bearer ${accessToken}`,
-            },
-        });
-        setResults(response.data);
-        setLoading(false);
+        const existingPlaylist = JSON.parse(localStorage.getItem("existingPlaylist"));
+        if (existingPlaylist) {
+          setResults(existingPlaylist);
+          setLoading(false);
+        } else {
+          let targetVariance = location.state ? location.state.targetVariance : 0.5;
+
+          targetVariance = Math.min(1, Math.max(0, parseFloat(targetVariance)));
+
+          /*const response = await axios.get("http://localhost:8888/results", {
+              headers: {
+                  Authorization: `Bearer ${accessToken}`,
+              },
+          });*/
+
+          const response = await axios.get(
+            `http://localhost:8888/recommendations?accessToken=${accessToken}&target_valence=${targetVariance}`
+          );
+
+          setResults(response.data);
+          setLoading(false);
+          localStorage.setItem("existingPlaylist", JSON.stringify(response.data));
+
+        }
+        
       } catch (err) {
         setError(true);
       }
@@ -25,10 +43,39 @@ function Result() {
     getResults();
   }, []);
 
+  /*<h1>Results</h1>
+  {results && (
+    <div>
+      <pre>{JSON.stringify(results, null, 2)}</pre>
+    </div>
+  )}*/
+
+  /*{results && (
+    <div>
+      <pre>{JSON.stringify(results, null, 2)}</pre>
+    </div> 
+  )}*/
 
   return (
     <>
-      <h1>Results</h1>
+     <h1>Results</h1>
+     <div>
+          <h2>Playlist name: {results.name}</h2>
+          {results.owner && (
+            <div>
+
+              <h2>Owner: {results.owner.display_name}</h2>
+            </div>
+          )}
+          {results.external_urls && (
+            <h2>Spotify Playlist Link: <a href={results.external_urls.spotify} target="_blank" rel="noopener noreferrer">{results.external_urls.spotify}</a></h2>
+          )}
+    
+          
+      </div>
+      
+      
+      
     </>
   );
 }
