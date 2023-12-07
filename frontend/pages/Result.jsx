b38be1266b79498a188825e8219b08d6776c67bf
Winnie Zhu <winniezhu@berkeley.edu>
2023-12-07 12:57:39 -0800

    name playlist based off of mood

--- frontend/pages/Result.jsx
+++ frontend/pages/Result.jsx
@@ -5,89 +5,87 @@ function Result() {
   const [results, setResults] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
-  
 
   const isMounted = useRef(false);
   useEffect(() => {
-    if (!isMounted.current){
-        isMounted.current = true;
-        const getResults = async () => {
+    if (!isMounted.current) {
+      isMounted.current = true;
+      const getResults = async () => {
         try {
-          const accessToken = localStorage.getItem('accessToken');
-          
-          
-          const existingPlaylist = JSON.parse(localStorage.getItem("existingPlaylist"));
+          const accessToken = localStorage.getItem("accessToken");
+
+          const existingPlaylist = JSON.parse(
+            localStorage.getItem("existingPlaylist")
+          );
+
+          const valence = localStorage.getItem("targetValence");
+          console.log("valence:", valence)
           console.log("Existing Playlist results page:", existingPlaylist);
           if (existingPlaylist) {
             setResults(existingPlaylist);
             setLoading(false); //
             return;
-  
-          } 
-            
-          let targetVariance = location.state ? location.state.targetVariance : 0.5;
-  
-          targetVariance = Math.min(1, Math.max(0, parseFloat(targetVariance)));
-  
+          }
+
+          console.log(`http://localhost:8888/recommendations?accessToken=${accessToken}&targetValence=${valence}`);
           const response = await axios.get(
-            `http://localhost:8888/recommendations?accessToken=${accessToken}&target_valence=${targetVariance}`
+            `http://localhost:8888/recommendations?accessToken=${accessToken}&targetValence=${valence}`
           );
-            
-            localStorage.setItem("existingPlaylist", JSON.stringify(response.data));
-            setResults(response.data);
-            
 
+          localStorage.setItem(
+            "existingPlaylist",
+            JSON.stringify(response.data)
+          );
+          setResults(response.data);
 
-
-
-            setLoading(false);
-
-           
-    
-        }catch (err) {
+          setLoading(false);
+        } catch (err) {
           setError(true);
-        }  
-      }
+        }
+      };
 
-          getResults();
-    }   
-    
+      getResults();
+    }
+
     //console.log('Results page- before calling getResults()');
-    
   }, []);
 
-
-    /*{results && (
+  /*{results && (
       <div>
         <pre>{JSON.stringify(results, null, 2)}</pre>
       </div> 
     )}*/
 
-  
   return (
     <>
-     <h1>Results</h1>
-     <div>
-          <h2>Playlist name: {results.name}</h2>
-          {results.owner && (
-            <div>
-
-              <h2>Owner: {results.owner.display_name}</h2>
-            </div>
-          )}
-          {results.external_urls && (
-                  <h2>Spotify Playlist Link: <a href={results.external_urls.spotify} target="_blank" rel="noopener noreferrer">{results.external_urls.spotify}</a></h2>
-
-          )}
+      <h1>Results</h1>
+      <div>
+        <h2>Playlist name: {results.name}</h2>
+        {results.owner && (
+          <div>
+            <h2>Owner: {results.owner.display_name}</h2>
+          </div>
+        )}
+        {results.external_urls && (
+          <h2>
+            Spotify Playlist Link:{" "}
+            <a
+              href={results.external_urls.spotify}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {results.external_urls.spotify}
+            </a>
+          </h2>
+        )}
       </div>
-      
+
       {loading && <p>Loading...</p>}
 
-      
       {results && !loading && !error && (
         <div>
           <iframe
-            style={{ borderRadius: '12px' }}
+            style={{ borderRadius: "12px" }}
             src={`https://open.spotify.com/embed/playlist/${results.id}?utm_source=generator`}
             width="100%"
             height="352"
@@ -98,10 +96,6 @@ function Result() {
           ></iframe>
         </div>
       )}
-      
-      
-      
-      
     </>
   );
 }
